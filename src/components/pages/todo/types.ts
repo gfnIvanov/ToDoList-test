cd293c0913eef71b1c798aaffe1f49d261b68ec4
Игорь Иванов <is.ivanov@bars.group>
2024-03-04 12:49:08 +0300

    Add sort

--- src/components/pages/todo/types.ts
+++ src/components/pages/todo/types.ts
@@ -3,3 +3,9 @@ import { TaskData } from '@/stores/types';
 export type AddEditTaskPayload = { task?: TaskData; taskData: TaskData };
 
 export type SetTaskStatusPayload = { checkedStatus: boolean; task: TaskData };
+
+export enum SortTypesValues {
+    COMPLETED = 'сначала выполненные',
+    INORDER = 'в порядке добавления',
+    UNFULFILLED = 'сначала не выполненные',
+}
